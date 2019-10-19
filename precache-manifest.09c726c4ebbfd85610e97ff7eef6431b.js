9ef59f8a553d0e11e8d3d834da647165bace4e43
Kat <e.genova@live.co.uk>
2019-10-19 17:36:33 +0100

    Updates

---
+++ precache-manifest.09c726c4ebbfd85610e97ff7eef6431b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55c8ddcb0d67269a999e5522160bd9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3fd53a88af637e683ab",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0e0d9e7a0654129b495e",
+    "url": "/static/js/2.66d6afa7.chunk.js"
+  },
+  {
+    "revision": "d3fd53a88af637e683ab",
+    "url": "/static/js/main.2f2f00fd.chunk.js"
+  },
+  {
+    "revision": "68f8a2c9d2b1c5471117",
+    "url": "/static/js/runtime-main.2febb294.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
